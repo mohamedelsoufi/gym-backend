9b61886066136c287e57cfd762a5685f7ec34e6f
Ibrahim El-Mokhtar <fsd72021@gmail.com>
2023-02-19 12:54:31 +0200

    Modify (Setting) model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -13,26 +13,74 @@ class Setting extends Model
 {
     use HasFactory, Translatable, HasFile, HasContact;
 
-    protected $table = 'settings';
+    protected $table = "settings";
 
     protected $guarded = [];
 
-    public $translatedAttributes = ['website_title', 'address','copyrights','meta_title','meta_description','footer_description'];
+    protected $appends = ["logo", "contact_img", "footer_img"];
+
+    public $translatedAttributes = [
+        "website_title",
+        "address",
+        "copyrights",
+        "meta_title",
+        "meta_description",
+        "footer_description",
+    ];
 
     public $timestamps = true;
 
-    public function getLogoAttribute(){
-        $logo = $this->file()->where('type','logo')->first();
+    public function getLogoAttribute()
+    {
+        $logo = $this->file()
+            ->where("type", "logo")
+            ->first();
         return $logo->path;
     }
 
-    public function getFooterImgAttribute(){
-        $img = $this->file()->where('type','footer_img')->first();
+    public function getFooterImgAttribute()
+    {
+        $img = $this->file()
+            ->where("type", "footer_img")
+            ->first();
         return $img->path;
     }
 
-    public function getContactImgAttribute(){
-        $img = $this->file()->where('type','contact_img')->first();
+    public function getContactImgAttribute()
+    {
+        $img = $this->file()
+            ->where("type", "contact_img")
+            ->first();
         return $img->path;
+    }
+
+    public function getWebsiteTitleAttribute($val)
+    {
+        return $this->attributes["website_title"] = ucwords($val);
+    }
+
+    public function getAddressAttribute($val)
+    {
+        return $this->attributes["address"] = ucwords($val);
+    }
+
+    public function getCopyrightsAttribute($val)
+    {
+        return $this->attributes["copyrights"] = ucwords($val);
+    }
+
+    public function getMetaTitleAttribute($val)
+    {
+        return $this->attributes["meta_title"] = ucwords($val);
+    }
+
+    public function getMetaDescriptionAttribute($val)
+    {
+        return $this->attributes["meta_description"] = ucwords($val);
+    }
+
+    public function getFooterDescriptionAttribute($val)
+    {
+        return $this->attributes["footer_description"] = ucwords($val);
     }
 }
