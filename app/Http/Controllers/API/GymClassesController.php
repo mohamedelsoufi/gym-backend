04f657d17378c430ed30fc5f3b92266fa8517b9b
deltaitech <deltaitech>
2023-07-11 09:23:15 +0300

    Edit GymClass API

--- app/Http/Controllers/API/GymClassesController.php
+++ app/Http/Controllers/API/GymClassesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ClassScheduleResource;
 use App\Http\Resources\GymClassResource;
 use App\Models\GymClass;
 use Illuminate\Http\Request;
@@ -19,7 +20,10 @@ class GymClassesController extends Controller
     {
         try {
             $gym_classes = GymClassResource::collection($this->gym_class->active()->latest('id')->get());
-            return successResponse($gym_classes, 'success', 200);
+            $class_schedules = $this->class_schedule->active()->get();
+            $data['gym_classes'] = GymClassResource::collection($gym_classes);
+            $data['class_schedules'] = ClassScheduleResource::collection($class_schedules);
+            return successResponse($data, 'success', 200);
         } catch (\Exception $e) {
             return failureResponse([], __('message.something_wrong'), 400);
         }
