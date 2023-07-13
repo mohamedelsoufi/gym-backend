eed41e9f67742bdc465ba4e0dc5755fb501f8ee7
deltaitech <deltaitech>
2023-07-13 14:48:06 +0300

    Edit blog API add events

--- app/Http/Controllers/API/BlogController.php
+++ app/Http/Controllers/API/BlogController.php
@@ -4,23 +4,28 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BlogResource;
+use App\Http\Resources\EventResource;
 use App\Models\Blog;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     private $blog;
+    private $event;
 
-    public function __construct(Blog $blog)
+    public function __construct(Blog $blog,Event $event)
     {
         $this->blog = $blog;
+        $this->event = $event;
     }
 
     public function __invoke()
     {
         try {
-            $blogs = BlogResource::collection($this->blog->active()->get());
-            return successResponse($blogs, 'success', 200);
+            $data['blogs'] = BlogResource::collection($this->blog->active()->get());
+            $data['events'] = EventResource::collection($this->event->active()->get());
+            return successResponse($data, 'success', 200);
         } catch (\Exception $e) {
             return failureResponse([], __('message.something_wrong'), 400);
         }
