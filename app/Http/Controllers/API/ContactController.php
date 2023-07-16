7df84bd9f6592c0be1bc79d9a46ad6a556aafdf2
deltaitech <deltaitech>
2023-07-16 13:54:02 +0300

    Implement Contact Request

--- app/Http/Controllers/API/ContactController.php
+++ app/Http/Controllers/API/ContactController.php
@@ -26,8 +26,7 @@ class ContactController extends Controller
             Mail::to(CONTACTS_MAIL)->send(new ContactMail($contact));
             return successResponse($contact, 'success', 200);
         } catch (\Exception $e) {
-            return redirect()->back()->with(['error' => $e->getMessage()]);
-
+            return failureResponse([],$e->getMessage(), 400);
             return failureResponse([], __('message.something_wrong'), 400);
         }
     }
