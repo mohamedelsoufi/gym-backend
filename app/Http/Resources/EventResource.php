635b058a7d279442c6848bd015a409f92ea63d7b
deltaitech <deltaitech>
2023-07-16 08:49:01 +0300

    Edit Event Resource

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -12,10 +12,10 @@ class EventResource extends JsonResource
             "id" => $this->id,
             "title" => $this->title,
             "description" => $this->description,
-            "duration" => $this->duration,
+            "duration" => __("words." . $this->duration),
             "date" => $this->date,
-            "from" =>  date('h:i A', strtotime($this->from)),
-            "to" =>  date('h:i A', strtotime($this->to)),
+            "from" => date('h:i A', strtotime($this->from)),
+            "to" => date('h:i A', strtotime($this->to)),
             "created_at" => $this->created_at,
         ];
     }
