dd47780d24987e74a1d1872d89414165c8c260cf
deltaitech <deltaitech>
2023-07-27 14:49:09 +0300

    Fix bug: remove unused modules

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -629,33 +629,33 @@
                 {{-- blog routes end --}}
 
                 {{-- comment routes start --}}
-                @permission('read-comments')
-                <li class="menu-item menu-item-submenu {{ request()->routeIs('comments.*') ? 'menu-item-open menu-item-here' : '' }}"
-                    aria-haspopup="true" data-menu-toggle="hover">
-                    <a href="javascript:;" class="menu-link menu-toggle">
-                        <i class="fas fa-comment-dots svg-icon menu-icon"></i>
-                        <span class="menu-text">{{__('words.comments')}}</span>
-                        <i class="menu-arrow"></i>
-                    </a>
-                    <div class="menu-submenu">
-                        <i class="menu-arrow"></i>
-                        <ul class="menu-subnav">
+{{--                @permission('read-comments')--}}
+{{--                <li class="menu-item menu-item-submenu {{ request()->routeIs('comments.*') ? 'menu-item-open menu-item-here' : '' }}"--}}
+{{--                    aria-haspopup="true" data-menu-toggle="hover">--}}
+{{--                    <a href="javascript:;" class="menu-link menu-toggle">--}}
+{{--                        <i class="fas fa-comment-dots svg-icon menu-icon"></i>--}}
+{{--                        <span class="menu-text">{{__('words.comments')}}</span>--}}
+{{--                        <i class="menu-arrow"></i>--}}
+{{--                    </a>--}}
+{{--                    <div class="menu-submenu">--}}
+{{--                        <i class="menu-arrow"></i>--}}
+{{--                        <ul class="menu-subnav">--}}
 
-                            @permission('read-comments')
-                            <li class="menu-item  {{ request()->routeIs('comments.index') ? 'menu-item-active' : '' }}"
-                                aria-haspopup="true">
-                                <a href="{{route('comments.index')}}" class="menu-link">
-                                    <i class="menu-bullet menu-bullet-dot">
-                                        <span></span>
-                                    </i>
-                                    <span class="menu-text">{{__('words.show_all')}}</span>
-                                </a>
-                            </li>
-                            @endpermission
-                        </ul>
-                    </div>
-                </li>
-                @endpermission
+{{--                            @permission('read-comments')--}}
+{{--                            <li class="menu-item  {{ request()->routeIs('comments.index') ? 'menu-item-active' : '' }}"--}}
+{{--                                aria-haspopup="true">--}}
+{{--                                <a href="{{route('comments.index')}}" class="menu-link">--}}
+{{--                                    <i class="menu-bullet menu-bullet-dot">--}}
+{{--                                        <span></span>--}}
+{{--                                    </i>--}}
+{{--                                    <span class="menu-text">{{__('words.show_all')}}</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+{{--                            @endpermission--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                </li>--}}
+{{--                @endpermission--}}
                 {{-- comment routes end --}}
 
                 {{-- event routes start --}}
@@ -695,7 +695,7 @@
                             </li>
                             @endpermission
 
-                            @permission('subscribes-events')
+                            @permission('subscribers-events')
                             <li class="menu-item  {{ request()->routeIs('events.subscribers') ? 'menu-item-active' : '' }}"
                                 aria-haspopup="true">
                                 <a href="{{route('events.subscribers')}}" class="menu-link">
